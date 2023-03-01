32110909b698c49a673c7390c41d5558816fc2fd
isokhan <igor.sokhan@sciplay.com>
2023-03-01 16:50:44 +0200

    initial commit

---
+++ lesson1-2/Assets/CarMovement.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float maxX = 5;
+    [SerializeField] private float maxZ = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Movement(Vector3.forward);
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            Movement(Vector3.back);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Movement(Vector3.left);
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            Movement(Vector3.right);
+        } 
+
+    }
+
+    private void Movement(Vector3 direction)
+    {
+        Vector3 newPosition = transform.position;
+        newPosition += direction * _speed * Time.deltaTime;
+
+        if (CheckIfPositionIsPossible(newPosition))
+        {
+            transform.position = newPosition;
+        }
+    }
+
+    private bool CheckIfPositionIsPossible(Vector3 newPosition)
+    {
+        if (Math.Abs(newPosition.x) > maxX)
+        {
+            return false;
+        }
+
+        if (Math.Abs(newPosition.z) > maxZ)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
